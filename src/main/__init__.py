59b1469f31476aec9d73bc34512759ee2456cc18
Dung Le <dle8@u.rochester.edu>
2020-01-03 00:59:26 +0700

    Connect to Cassandra db

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from src.main.config import Config
 from flask_mail import Mail
+from cassandra.cluster import Cluster
 
 app = Flask(__name__, template_folder='../../templates')
 app.config.from_object(config)
@@ -16,5 +17,9 @@ mail.init_app(app)
 
 cors = CORS()
 cors.init_app(app)
+
+# Connect to Cassandra db with 'Kronos' keyspace
+cluster = Cluster()
+session = cluster.connect('Kronos')
 
 _register_subpackages()
