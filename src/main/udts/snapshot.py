325e70a949f242460ff071acde56aeedfe90615b
Dung Le <dle8@u.rochester.edu>
2020-01-04 21:12:11 +0700

    Define type snapshot

---
+++ src/main/udts/snapshot.py
@@ -0,0 +1,15 @@
+from src.main import cluster
+from cassandra.cqlengine.columns import *
+from . import article
+from datetime import datetime
+import uuid
+
+
+class Snapshot(object):
+    id = UUID(default=uuid.uuid4())
+    start = Date(default=datetime.now().date())
+    end = Date(default=datetime.now().date())
+    articles = List(UserDefinedType(article))
+
+
+cluster.register_user_type('kronos', 'snapshot', Snapshot)
