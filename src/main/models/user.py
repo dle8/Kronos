23a5dc907f26ad7982c79b1deaa07253029989db
Dung Le <dle8@u.rochester.edu>
2020-01-06 16:57:25 +0700

    Add method to validate user email

--- src/main/models/user.py
+++ src/main/models/user.py
@@ -4,6 +4,8 @@ from cassandra.cqlengine.management import sync_table
 from src.main.udts import *
 from src.main.config import config
 import uuid
+import requests
+import json
 
 
 class User(Model):
@@ -14,6 +16,18 @@ class User(Model):
     stocks = Set(UserDefinedType(stock))
     tags = Set(UserDefinedType(tag))
     articles = Set(UserDefinedType(article))
+
+    def validate(self):
+        super(User, self).validate()
+        if self.email:
+            api_response = requests.post(
+                config.EMAIL_VERIFICATION_URL.format(config.API_KEY, self.email)
+            ).content
+            api_response = json.loads(api_response)
+
+            if api_response['result'] == 'invalid':
+                # raise Exception('Invalid email')
+                pass
 
 
 sync_table(User)
