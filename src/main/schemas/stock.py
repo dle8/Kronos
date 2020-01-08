841db26089603f0cb3bce08de3e889fe0e716b21
Dung Le <dle8@u.rochester.edu>
2020-01-09 02:04:49 +0700

    Refactor resovler functions

--- src/main/schemas/stock.py
+++ src/main/schemas/stock.py
@@ -1,28 +1,46 @@
-from graphene import UUID, String, Float, Int, ObjectType
-from src.main.schemas.base_schema import ID
+from graphene import String, Float, Int, ObjectType, Field
+from src.main.libs.alpha_advantage.query_stock import fetch_stock_prices
+from . import data
 
 
 class Stock(ObjectType):
-    class Meta:
-        interfaces = (ID,)
-
-    id = UUID(required=True)
     name = String(required=True)
-    ticker_symbol = String(required=True)
-    price = Int(required=True)
-    percentage_variation = Float(required=True)
+    symbol = String(required=True, symbol=String())  # Index in Cassandra
+    price = Int()
+    price_difference = Float()
 
-    def resolve_id(self, info, **kwargs):
-        return self.id
+    @staticmethod
+    def resolve_name(parent, info, **kwargs):
+        stock = data[parent.symbol]
+        name = stock.get('Name', None)
+        if not name:
+            raise Exception('Stock whose symbol is {} does not exist.'.format(parent.symbol))
+        return name
 
-    def resolve_name(self, info, **kwargs):
-        return self.name
+    @staticmethod
+    def resolve_symbol(parent, info, **kwargs):
+        return parent.symbol
 
-    def resolve_ticker_symbol(self, info, **kwargs):
-        return self.ticker_symbol
+    # Query stock intra day price for each 1 min
+    @staticmethod
+    def resolve_price(parent, info, **kwargs):
+        prices = fetch_stock_prices(parent.symbol)
+        list_prices = list(prices.items())
+        price = list_prices[0][1]['4. close']
 
-    def resolve_price(self, info, **kwargs):
-        return self.price
+        return price
 
-    def resolve_percentage_variation(self, info, **kwargs):
-        return self.percentage_variation
+    @staticmethod
+    def resolve_price_difference(parent, info, **kwargs):
+        prices = fetch_stock_prices(parent.symbol)
+        last_close_price = prices[0]['4. close']
+
+        price_difference = 0
+        for i in range(len(prices) - 1):
+            nearest_trade_time = prices[i][0].split(' ')[0]
+            previous_trade_time = prices[i + 1][0].split(' ')[0]
+            if previous_trade_time != nearest_trade_time:
+                price_difference = prices[i + 1][1]['4. close'] - last_close_price
+                break
+
+        return price_difference
