d6c1f335b21ef0df1122d25bfab860c6700b22e5
Dung Le <dle8@u.rochester.edu>
2020-01-08 21:15:29 +0700

    Refactor email validation to /libs directory. Set email to primary key and adjust field names:

--- src/main/models/user.py
+++ src/main/models/user.py
@@ -1,33 +1,24 @@
 from cassandra.cqlengine.columns import *
 from cassandra.cqlengine.models import Model
 from cassandra.cqlengine.management import sync_table
-from src.main.udts import *
+from src.main.udts.tag import Tag
+from src.main.udts.article import Article
 from src.main.config import config
-import uuid
-import requests
-import json
+from src.main.libs.neverbounce.verify_email import validate_email
 
 
 class User(Model):
     __keyspace__ = config.KEYSPACE  # CASSANDRA KEYSPACE
-    id = UUID(default=uuid.uuid4())
-    email = Text(required=True)
+    email = Text(required=True, primary_key=True)
     hashed_password = Text(required=True)
-    stocks = Set(UserDefinedType(stock))
-    tags = Set(UserDefinedType(tag))
-    articles = Set(UserDefinedType(article))
+    stock_symbols = Set(Text)
+    tag_names = Set(UserDefinedType(Tag))
+    articles = Set(UserDefinedType(Article))
 
     def validate(self):
         super(User, self).validate()
         if self.email:
-            api_response = requests.post(
-                config.EMAIL_VERIFICATION_URL.format(config.API_KEY, self.email)
-            ).content
-            api_response = json.loads(api_response)
-
-            if api_response['result'] == 'invalid':
-                # raise Exception('Invalid email')
-                pass
+            validate_email(self.email)
 
 
 sync_table(User)
