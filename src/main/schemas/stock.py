ce37180041e46f683c9e82a4c0c8af4bcc778bd8
Dung Le <dle8@u.rochester.edu>
2020-01-09 19:48:31 +0700

    Refactor code with fetch_stock_all_methods decorator & add resolve articles. Remove fetching price difference

--- src/main/schemas/stock.py
+++ src/main/schemas/stock.py
@@ -1,46 +1,53 @@
-from graphene import String, Float, Int, ObjectType, Field
+from graphene import String, Int, ObjectType, List, NonNull, Date
 from src.main.libs.alpha_advantage.query_stock import fetch_stock_prices
-from . import data
+from src.main.schemas.article import ArticleType
+from src.main.libs.google_search.fetch_article_metadata import fetch_url_metadata
+from src.main.utils.fetch_stock import fetch_stock_all_methods
+
+from datetime import datetime
 
 
+@fetch_stock_all_methods
 class StockType(ObjectType):
     name = String(required=True)
     symbol = String(required=True, symbol=String())  # Index in Cassandra
     price = Int()
-    price_difference = Float()
+    articles = List(
+        NonNull(ArticleType),
+        start_date=Date(),
+        end_data=Date(default_value=datetime.now().date()),
+        start=Int(default_value=1)
+    )  # start for pagination
 
     @staticmethod
     def resolve_name(parent, info, **kwargs):
-        stock = data[parent.symbol]
-        name = stock.get('Name', None)
-        if not name:
-            raise Exception('Stock whose symbol is {} does not exist.'.format(parent.symbol))
-        return name
+        return kwargs['name']
 
     @staticmethod
     def resolve_symbol(parent, info, **kwargs):
-        return parent.symbol
+        return kwargs['symbol']
 
     # Query stock intra day price for each 1 min
     @staticmethod
     def resolve_price(parent, info, **kwargs):
-        prices = fetch_stock_prices(parent.symbol)
+        prices = fetch_stock_prices(kwargs['symbol'])
         list_prices = list(prices.items())
         price = list_prices[0][1]['4. close']
 
         return price
 
     @staticmethod
-    def resolve_price_difference(parent, info, **kwargs):
-        prices = fetch_stock_prices(parent.symbol)
-        last_close_price = prices[0]['4. close']
+    def resolve_articles(parent, info, **kwargs):
+        # Todo: Try different search term & use NLP to pick out the best
+        time_range = 'date:r:'
+        if kwargs.get('start_date', None):
+            time_range += str(kwargs['start_date'])
+        time_range += ':' + kwargs['end_date']
 
-        price_difference = 0
-        for i in range(len(prices) - 1):
-            nearest_trade_time = prices[i][0].split(' ')[0]
-            previous_trade_time = prices[i + 1][0].split(' ')[0]
-            if previous_trade_time != nearest_trade_time:
-                price_difference = prices[i + 1][1]['4. close'] - last_close_price
-                break
+        articles = fetch_url_metadata(
+            kwargs['name'] + ' market news',
+            start=kwargs['start'],
+            sort=time_range
+        )
 
-        return price_difference
+        return [ArticleType(article=article) for article in articles]
