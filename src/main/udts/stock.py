b9711a6baaf5f1b64f03d577d4476a6202b2cf7c
Dung Le <dle8@u.rochester.edu>
2020-01-09 19:56:04 +0700

    Remove unusued fields

--- src/main/udts/stock.py
+++ src/main/udts/stock.py
@@ -1,14 +1,10 @@
-import uuid
 from src.main import cluster
 from cassandra.cqlengine.columns import *
 
 
 class Stock(object):
-    id = UUID(default=uuid.uuid4())
     name = Text(required=True)
-    ticker_symbol = Text(required=True)
-    price = Integer(required=True)
-    percentage_variation = Float(required=True)
+    symbol = Text(required=True)
 
 
 cluster.register_user_type('kronos', 'stock', Stock)
