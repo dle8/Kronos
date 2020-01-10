80286af1cb2364afa755789fa4262a464247bad8
Dung Le <dle8@u.rochester.edu>
2020-01-10 21:44:17 +0700

    Refactor code fetching user to __init__

--- src/main/utils/fetch_user.py
+++ src/main/utils/fetch_user.py
@@ -1,16 +1,13 @@
 from functools import wraps
 from src.main.models.user import User
+from inspect import getattr_static
 
 
 def fetch_user(f, **fields):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        email = fields['email']
-        user = User.objects.filter(email=email).first()
-        if not user:
-            raise Exception('Invalid email or password.')
+        kwargs['user'] = fields['user']
 
-        kwargs['user'] = user
         return f(*args, **kwargs)
 
     return wrapper
@@ -20,16 +17,12 @@ def fetch_user_all_methods(Cls):
     class NewCls(object):
 
         def __init__(self, *args, **kwargs):
-            self.email = kwargs['email']
+            self.user = User.objects.filter(email=kwargs['email']).first()
+            if not self.user:
+                raise Exception('Invalid email or password.')
             self.oInstance = Cls(*args, **kwargs)
 
         def __getattribute__(self, s):
-            """
-            this is called whenever any attribute of a NewCls object is accessed. This function first tries to
-            get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
-            instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and
-            the attribute is an instance method then `time_this` is applied.
-            """
             try:
                 x = super(NewCls, self).__getattribute__(s)
             except AttributeError:
@@ -37,8 +30,8 @@ def fetch_user_all_methods(Cls):
             else:
                 return x
             x = self.oInstance.__getattribute__(s)
-            if type(x) == type(self.__init__):  # it is an instance method
-                return fetch_user(x, email=self.email)
+            if isinstance(getattr_static(Cls, s), staticmethod):
+                return fetch_user(x, user=self.user)
             else:
                 return x
 
