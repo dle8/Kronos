3c09ee5e002b76c066fdb4ddd4329cabbcf291f6
Dung Le <dle8@u.rochester.edu>
2020-01-10 22:52:53 +0700

    Add more fields to query stocks

--- src/main/schemas/query.py
+++ src/main/schemas/query.py
@@ -1,4 +1,4 @@
-from graphene import ObjectType, List, NonNull, Field, Int, String
+from graphene import ObjectType, List, NonNull, Field, Int, String, Date
 from src.main.schemas.stock import StockType
 from src.main.schemas.user import UserType
 
@@ -6,7 +6,14 @@ from flask import session
 
 
 class Query(ObjectType):
-    stocks = Field(List(NonNull(StockType)), first=Int(), offset=Int(), symbol=String(required=True))
+    stocks = Field(List(
+        NonNull(StockType)),
+        first=Int(),
+        offset=Int(),
+        symbol=String(required=True),
+        start_date=Date(),
+        end_date=Date()
+    )
     user = Field(String, email=String(required=True))
 
     @staticmethod
@@ -20,7 +27,7 @@ class Query(ObjectType):
     @staticmethod
     def resolve_stocks(parent, info, **kwargs):
         if kwargs.get('symbol', None):
-            return [StockType(symbol=kwargs['symbol'])]
+            return [StockType(**kwargs)]
 
         # Todo: Use memcached for most recent searched stocks, here is just hardcoding
         return [StockType(symbol=symbol) for symbol in ['AAPL', 'FB', 'MSFT', 'GOOGL', 'AMZN']]
